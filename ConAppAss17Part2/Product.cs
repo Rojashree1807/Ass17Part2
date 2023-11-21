19552d1d70882d269df59e315d0b48628438ebda
Rojashree1807 <rojarojashree1807@gmail.com>
2023-11-21 15:50:18 +0530

    first commit

---
+++ ConAppAss17Part2/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAss17Part2
+{
+         public class Product
+        {
+            protected int pid;
+            protected string pname;
+            protected double pprice;
+
+            public void ShowDetails()
+            {
+                Console.WriteLine($"Product ID: {pid}");
+                Console.WriteLine($"Product Name: {pname}");
+                Console.WriteLine($"Product Price: {pprice:C}");
+            }
+
+            public void SetInformation(int id, string name, double price)
+            {
+                pid = id;
+                pname = name;
+                pprice = price;
+            }
+        }
+    
+}
---
+++ ConAppAss17Part2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAss17Part2
+{
+    public  class Program
+    {
+      
+            static void Main()
+            {
+                HerbalProduct herbalProduct = new HerbalProduct();
+
+                
+                herbalProduct.SetInformation(101, "Herbal Shampoo", 12.99, "Aloe Vera", DateTime.Parse("2023-01-01"), DateTime.Parse("2024-01-01"));
+
+                Console.WriteLine("Herbal Product Details:");
+                herbalProduct.ShowDetails();
+                 Console.ReadKey();
+            }
+    }
+
+}
+
